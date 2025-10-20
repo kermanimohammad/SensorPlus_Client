0344dac36ac2b61ffa691ec726a7e0b80750df85
Mohammad Mehdi Kermani <40020573+kermanimohammad@users.noreply.github.com>
2025-10-20 17:57:12 -0400

    Reset

--- start-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting DigitalTwin Database Server..."
-echo ""
-echo "Make sure you have Node.js installed and dependencies are installed with:"
-echo "npm install"
-echo ""
-echo "Starting server on port 3001..."
-echo ""
-node server.js
